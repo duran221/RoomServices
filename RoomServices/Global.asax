c7fdbc43be289a60ecc59f48483abfae795e5e32
duran221 <cristianfernandoduran@gmail.com>
2020-03-06 16:31:29 -0500

    Primer cambio

---
+++ RoomServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoomServices.MvcApplication" Language="C#" %>
---
+++ RoomServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoomServices.Startup))]
+namespace RoomServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
